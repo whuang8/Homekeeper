862d21d859300bcbcfa643555efa5604819650c2
GraysonR <gricket@purdue.edu>
2015-10-10 21:13:52 -0400

    First GroceryList commit.

---
+++ Homekeeper/GroceryItem.swift
@@ -0,0 +1,13 @@
+//
+//  GroceryItem.swift
+//  Homekeeper
+//
+//  Created by Grayson Ricketts on 10/10/15.
+//  Copyright © 2015 Team9. All rights reserved.
+//
+
+import UIKit
+
+class GroceryItem {
+    // Mark: Properties
+}
