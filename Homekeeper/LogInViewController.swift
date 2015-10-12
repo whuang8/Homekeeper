7bca654d5d532a48e1dd6e72c0199731f11bbeea
whuang8 <huang554@purdue.edu>
2015-10-12 19:05:50 -0400

    Connected UI to backend user authentication system

--- Homekeeper/LogInViewController.swift
+++ Homekeeper/LogInViewController.swift
@@ -7,30 +7,74 @@
 //
 
 import UIKit
+import Firebase
 
 
-class LogInViewController: UIViewController {
+class LogInViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var emailText: UITextField!
     @IBOutlet weak var passwordText: UITextField!
     
+    var loggedIn: Bool = false
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        //Looks for single or multiple taps.
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "DismissKeyboard")
+        view.addGestureRecognizer(tap)
   
     }
     
     //runs when login button is hit
     @IBAction func logIn(sender: UIButton) {
-        
         checkTextFields()
+        let ref = Firebase(url: "https://homekeeper.firebaseio.com/")
+        ref.authUser(emailText.text, password: passwordText.text,
+            withCompletionBlock: { error, authData in
+                if error != nil {
+                    // There was an error logging in to this account
+                    print("Error in log in")
+                    self.emailText.text = ""
+                    self.passwordText.text = ""
+                    self.loggedIn = false
+                    
+                    let title = "Wrong Login Credentials"
+                    let message = "Please try again."
+                    // Initialize Alert Controller
+                    let alertController = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+                    
+                    // Initialize Actions
+                    let okAction = UIAlertAction(title: "Ok", style: .Default) { (action) -> Void in
+                        print("Ok")
+                    }
+                    
+                    // Add Actions
+                    alertController.addAction(okAction)
+                    
+                    // Present Alert Controller
+                    self.presentViewController(alertController, animated: true, completion: nil)
+                    
+                } else {
+                    // We are now logged in
+                    print("Log in successful")
+                    self.loggedIn = true
+                    self.performSegueWithIdentifier("SignInSegue", sender: self)
+                }
+        })
+    }
+    
+    override func shouldPerformSegueWithIdentifier(identifier: String?, sender: AnyObject?) -> Bool {
         
+        let performSegue = self.loggedIn
+        self.loggedIn = false
+        return performSegue
     }
     
     //runs when sign up button is hit
     @IBAction func signUp(sender: UIButton) {
-        
-        
+        self.performSegueWithIdentifier("SignUpSegue", sender: self)
     }
     
     func checkTextFields(){
@@ -69,6 +113,12 @@ class LogInViewController: UIViewController {
             self.presentViewController(alertController, animated: true, completion: nil)
             
         }
+    }
+    
+    
+    func DismissKeyboard(){
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
     }
     
     override func didReceiveMemoryWarning() {
