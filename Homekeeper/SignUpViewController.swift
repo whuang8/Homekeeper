25684d8c2a2ab84195c31ec56d2e65e29a075975
TannerStrom <tstrom@purdue.edu>
2015-12-07 23:58:10 -0500

    Signup page is almost finished, just need to add a background, same with login

--- Homekeeper/SignUpViewController.swift
+++ Homekeeper/SignUpViewController.swift
@@ -15,6 +15,8 @@ class SignUpViewController: UIViewController {
     @IBOutlet weak var lastName: UITextField!
     @IBOutlet weak var email: UITextField!
     @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var submit: UIButton!
+    @IBOutlet weak var cancel: UIButton!
     
     var key=""
     var created: Bool = false
@@ -73,6 +75,77 @@ class SignUpViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //Start Tanner's stuff here
+    
+    
+    @IBAction func cancelAction(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil);
+    }
+    
+    override func viewDidLayoutSubviews() {
+        let border = CALayer()
+        let width = CGFloat(1.0)
+        border.borderColor = UIColor.darkGrayColor().CGColor
+        border.frame = CGRect(x: 0,
+            y: firstName.frame.size.height - width,
+            width:  firstName.frame.size.width,
+            height: firstName.frame.size.height)
+        
+        border.borderWidth = width
+        firstName.layer.addSublayer(border)
+        firstName.layer.masksToBounds = true
+        firstName.clipsToBounds = true;
+        
+        let border1 = CALayer()
+        //let width = CGFloat(1.0)
+        border1.borderColor = UIColor.darkGrayColor().CGColor
+        border1.frame = CGRect(x: 0,
+            y: email.frame.size.height - width,
+            width:  email.frame.size.width,
+            height: email.frame.size.height)
+        
+        border1.borderWidth = width
+        email.layer.addSublayer(border1)
+        email.layer.masksToBounds = true
+        email.clipsToBounds = true;
+        
+        let border2 = CALayer()
+        //let width = CGFloat(1.0)
+        border2.borderColor = UIColor.darkGrayColor().CGColor
+        border2.frame = CGRect(x: 0,
+            y: lastName.frame.size.height - width,
+            width:  lastName.frame.size.width,
+            height: lastName.frame.size.height)
+        
+        border2.borderWidth = width
+        lastName.layer.addSublayer(border2)
+        lastName.layer.masksToBounds = true
+        lastName.clipsToBounds = true;
+        
+        let border3 = CALayer()
+        //let width = CGFloat(1.0)
+        border3.borderColor = UIColor.darkGrayColor().CGColor
+        border3.frame = CGRect(x: 0,
+            y: password.frame.size.height - width,
+            width:  password.frame.size.width,
+            height: password.frame.size.height)
+        
+        border3.borderWidth = width
+        password.layer.addSublayer(border3)
+        password.layer.masksToBounds = true
+        password.clipsToBounds = true;
+        
+        submit.backgroundColor = UIColor(red: 0.0, green: 122.0/255.0, blue:1.0, alpha: 1.0);
+        submit.tintColor = UIColor.whiteColor();
+        submit.alpha = 0.50;
+        submit.layer.cornerRadius = 10
+        ;
+        submit.clipsToBounds = true;
+        
+        cancel.setTitle("Cancel", forState: .Normal);
+        
     }
     
 
